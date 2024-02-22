facb93e45a5c7b16071f4ce4a1023fb5c42a72a5
Ch Hanan Hadayat <hanani.nas@gmail.com>
2024-02-22 15:53:43 +0100

    Added vscode files for debugging

--- DumAI.java
+++ DumAI.java
@@ -1,22 +1,25 @@
+import java.io.Console;
 import java.util.ArrayList;
 
 /**
  * A simple OthelloAI-implementation. The method to decide the next move just
- * returns the first legal move that it finds. 
+ * returns the first legal move that it finds.
+ * 
  * @author Mai Ajspur
  * @version 9.2.2018
  */
-public class DumAI implements IOthelloAI{
+public class DumAI implements IOthelloAI {
 
 	/**
 	 * Returns first legal move
 	 */
-	public Position decideMove(GameState s){
+	public Position decideMove(GameState s) {
 		ArrayList<Position> moves = s.legalMoves();
-		if ( !moves.isEmpty() )
+		if (!moves.isEmpty())
 			return moves.get(0);
 		else
-			return new Position(-1,-1);
+			return new Position(-1, -1); // this function is just a simple ai that goes one postion down and up
+
 	}
-	
+
 }
